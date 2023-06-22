a3c3836fdd7bb06e23363d1eb61ac08dfa4278f9
Abhishek-90 <holaniabhishek90@gmail.com>
2023-06-22 22:36:36 +0530

    added signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { Container, Stack } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function Signup() {
+  const [signupCredentials, setSignupCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <Container className="mt-4">
+      <Stack gap={4}>
+        <h2 className="mx-auto">Appwrite | Expense Manager</h2>
+        <Form className="mt-4">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              name="name"
+              type="text"
+              placeholder="Enter name"
+              onChange={(e) =>
+                setSignupCredentials({
+                  ...signupCredentials,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              value={signupCredentials.name}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) =>
+                setSignupCredentials({
+                  ...signupCredentials,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              value={signupCredentials.email}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setSignupCredentials({
+                  ...signupCredentials,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              value={signupCredentials.password}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Stack>
+    </Container>
+  );
+}
+
+export default Signup;
