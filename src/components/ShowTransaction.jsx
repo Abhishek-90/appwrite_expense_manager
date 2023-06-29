4dc45734fd09771f4877ef9e6daac8c4e78c92c2
Abhishek-90 <holaniabhishek90@gmail.com>
2023-06-29 16:47:22 +0530

    update modal edit button

--- src/components/ShowTransaction.jsx
+++ src/components/ShowTransaction.jsx
@@ -1,9 +1,16 @@
-import { Container } from "react-bootstrap";
+import { Container, Stack } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
+import { FiEdit } from "react-icons/fi";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useState } from "react";
+import UpdateModal from "./UpdateModal";
 
 export default function ShowTransaction() {
+  const [showEditModal, setShowEditModal] = useState(false);
+
   return (
     <Container className="mt-3">
+      {showEditModal && <UpdateModal setShowEditModal={setShowEditModal} />}
       <Table>
         <thead>
           <tr>
@@ -20,7 +27,19 @@ export default function ShowTransaction() {
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
-            <td>@mdo</td>
+            <td>
+              <Stack direction="horizontal" gap={3}>
+                <button
+                  className="action-button"
+                  onClick={() => setShowEditModal(true)}
+                >
+                  <FiEdit className="icon" />
+                </button>
+                <button className="action-button">
+                  <AiOutlineDelete className="icon delete-icon" />
+                </button>
+              </Stack>
+            </td>
           </tr>
         </tbody>
       </Table>
