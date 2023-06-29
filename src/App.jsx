b05798b641f8aabba9ecfbf4dacfe3df0b52e2e5
Abhishek-90 <holaniabhishek90@gmail.com>
2023-06-29 13:15:19 +0530

    app created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import Login from "./pages/Login";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Signup from "./pages/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
